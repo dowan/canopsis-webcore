72376103349e46cc7355fb201a7007b7c83a0b96
utopman <eregnier@capensis.fr>
2014-09-10 16:10:05 +0200

    update: time display management

--- var/www/canopsis/core/lib/helpers/timestamp.js
+++ var/www/canopsis/core/lib/helpers/timestamp.js
@@ -22,9 +22,18 @@ define(['ember', 'utils'], function(Ember, utils) {
     Ember.Handlebars.helper('timestamp', function(value, attr , record) {
 
         value = record.timeStampState || value;
+
+        var current = new Date().getTime();
+        var timestamp = new Date(value * 1000);
+        var timeSince = utils.dates.diffDate(timestamp, current, "d") - 1;
+
         var time ="";
         if(value)
-             time = utils.dates.timestamp2String(value, attr.options.format, true);
+            var format = attr.options.format;
+            if (timeSince === 0) {
+                format = 'timeOnly';
+            }
+            time = utils.dates.timestamp2String(value, format, true);
 
         return new Ember.Handlebars.SafeString(time);
     });
