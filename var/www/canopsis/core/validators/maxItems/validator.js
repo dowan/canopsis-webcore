03baf3d166fe59fa835f6207c9faeddf53284368
Gwenaël Pluchon <gpluchon@capensis.fr>
2014-09-23 16:37:14 +0200

    fix bad code smell in validators

--- var/www/canopsis/core/validators/maxItems/validator.js
+++ var/www/canopsis/core/validators/maxItems/validator.js
@@ -1,22 +1,35 @@
-define([
-], function() {
+/*
+# Copyright (c) 2014 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis. If not, see <http://www.gnu.org/licenses/>.
+*/
 
-  function maxItems(attr, valideStruct)
-  {
-    var maxItems = attr.model.options.maxItems;
-    if(attr.value.length <= maxItems){
+define([], function() {
 
-      valideStruct.valid = true ;
+    function maxItemsValidator(attr, valideStruct) {
+        var maxItemsModelOption = attr.model.options.maxItems;
 
+        if(attr.value.length <= maxItemsModelOption){
+            valideStruct.valid = true ;
+        } else {
+            valideStruct.valid = false ;
+            valideStruct.error = "There can be at most " + maxItemsModelOption + " item(s).";
+        }
+        return valideStruct;
     }
-    else{
 
-        valideStruct.valid = false ;
-        valideStruct.error = "There can be at most " + maxItems + " item(s).";
-
-    }
-    return valideStruct;
-  }
-
-  return maxItems;
+    return maxItemsValidator;
 });
