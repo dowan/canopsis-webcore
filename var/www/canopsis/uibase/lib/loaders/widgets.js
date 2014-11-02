fb8bc06dd09170cf703e48d05eb7d66f8f3bc654
mrbozzo <jlabejof@yahoo.fr>
2014-11-02 17:06:39 +0100

    :fix: bug in application template in adding condition on displaying header/footer parts if they exist

--- var/www/canopsis/uibase/lib/loaders/widgets.js
+++ var/www/canopsis/uibase/lib/loaders/widgets.js
@@ -33,7 +33,6 @@ var widgets = [
     { name:'euewi', url:'canopsis/uibase/widgets/euewi/controller', template:'canopsis/uibase/widgets/list/template.html', TEMPLATE: 'list' },
     { name:'timegraph', url:'canopsis/uibase/widgets/timegraph/controller', template:'canopsis/uibase/widgets/timegraph/template.html'},
     { name:'progressbar', url:'canopsis/uibase/widgets/progressbar/controller', template:'canopsis/uibase/widgets/progressbar/template.html'},
-    { name:'graph', url:'canopsis/uibase/widgets/graph/controller', template:'canopsis/uibase/widgets/graph/template.html' },
     { name:'graph', url:'canopsis/uibase/widgets/graph/controller', template:'canopsis/uibase/widgets/graph/template.html' }
     //{ name:'gauge', url:'canopsis/uibase/widgets/gauge'}
 
--- var/www/canopsis/uibase/widgets/graph/controller.js
+++ var/www/canopsis/uibase/widgets/graph/controller.js
@@ -177,10 +177,16 @@ define([
             function select(d) {
                 if (d.selected) {
                     d.selected = false;
-                    d3.select(this).style('stroke-width', 1);
+                    d3.select(this)
+                        .transition().duration(300)
+                            .style('stroke-width', 1)
+                            .style('stroke', 'black');
                 } else {
                     d.selected = true;
-                    d3.select(this).style('stroke-width', 2);
+                    d3.select(this)
+                        .transition().duration(300)
+                            .style('stroke-width', 2)
+                            .style('stroke', 'blue');
                 }
             }
             function gotoeventpool(d){
@@ -246,53 +252,6 @@ define([
             d3.select('.graphplay').on('click', playtransition);
 
         },
-
-        displayjsplumb: function() {
-            debugger;
-            jsPlumb.ready(function() {
-
-              var i = 0;
-
-              $('#container').dblclick(function(e) {
-                var newState = $('<div>').attr('id', 'state' + i).addClass('item');
-
-                var title = $('<div>').addClass('title').text('State ' + i);
-                var connect = $('<div>').addClass('connect');
-
-                newState.css({
-                  'top': e.pageY,
-                  'left': e.pageX
-                });
-
-                jsPlumb.makeTarget(newState, {
-                  anchor: 'Continuous'
-                });
-
-                jsPlumb.draggable(newState, {
-                  containment: 'parent'
-                });
-
-                jsPlumb.makeSource(connect, {
-                  parent: newState,
-                  anchor: 'Continuous'
-                });
-
-                newState.dblclick(function(e) {
-                  jsPlumb.detachAllConnections($(this));
-                  $(this).remove();
-                  e.stopPropagation();
-                });
-
-                newState.append(title);
-                newState.append(connect);
-
-                $('#container').append(newState);
-
-                i++;
-              });
-
-            });
-        }.property(),
 
         displaycy: function(nodes) {
             debugger;
