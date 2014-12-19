c5e32ee213bf9e34fb17849bbdd48118202374fc
Gwenaël Pluchon <gpluchon@capensis.fr>
2014-12-19 10:38:45 +0100

    fix event list duration column

--- var/www/canopsis/uibase/widgets/list/controller.js
+++ var/www/canopsis/uibase/widgets/list/controller.js
@@ -140,7 +140,7 @@ define([
 
                     if(get(currentColumn, 'options.show')) {
                         if(currentColumn.renderer) {
-                            html += '<td class="' + currentColumn.field + '">{{component-renderer rendererType="' + currentColumn.renderer + '" value=this.'+ currentColumn.field +'}}</td>';
+                            html += '<td class="' + currentColumn.field + '">{{component-renderer rendererType="' + currentColumn.renderer + '" value=this.'+ currentColumn.field +' record=this}}</td>';
                         } else {
                             html += '<td class="' + currentColumn.field + '">{{this.'+ currentColumn.field + '}}</td>';
                         }
