b7e0c7d734c7569b0c44138814ffb64e3432081f
Gwenaël Pluchon <gpluchon@capensis.fr>
2015-01-22 11:15:26 +0100

    add missing headers

--- var/www/canopsis/canopsisConfiguration.js
+++ var/www/canopsis/canopsisConfiguration.js
@@ -1,3 +1,22 @@
+/*
+# Copyright (c) 2014 "Capensis" [http://www.capensis.com]
+#
+# This file is part of Canopsis.
+#
+# Canopsis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canopsis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Canopsis. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 define([], function () {
 
     /*
@@ -17,11 +36,12 @@ define([], function () {
 
 
     if(canopsisConfiguration.DEBUG === false) {
-        console.log = function() {};
-        console.warn = function() {};
-        console.debug = function() {};
-        console.group = function() {};
-        console.groupEnd = function() {};
+        console.log = function(){};
+        console.info = function(){};
+        console.error = function(){};
+        console.group = function(){};
+        console.groupEnd = function(){};
+        console.warn = function(){};
     }
 
     return canopsisConfiguration;
