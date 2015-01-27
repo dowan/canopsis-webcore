7ea73d8c24b5ff61a5abe6201e88f41c7fc87289
srassinoux <srassinoux@gmail.com>
2015-01-27 22:33:37 +0100

    update editor conf

--- var/www/canopsis/monitoring/widgets/progressbar/controller.js
+++ var/www/canopsis/monitoring/widgets/progressbar/controller.js
@@ -18,49 +18,32 @@
 */
 
 define([
-	'jquery',
-	//'ember',
-    //'ember-data',
-    //'app/lib/utils/values',
-	'app/lib/factories/widget',
-	'app/controller/perfdata',
-	'canopsis/uibase/components/progressbar/component'
+    'jquery',
+    'app/lib/factories/widget',
+    'app/controller/perfdata',
+    'app/controller/serie',
+    'canopsis/uibase/components/progressbar/component'
 ], 
 function($, WidgetFactory) {
 
-	var get = Ember.get,
-	set = Ember.set;
+    var get = Ember.get,
+    set = Ember.set;
 
-	var widgetOptions = {};
+    var widgetOptions = {};
 
-	var widget = WidgetFactory('progressbar', {
+    var widget = WidgetFactory('progressbar', {
 
-		needs: ['serie', 'perfdata'],
-
-		init: function() {
-			this._super();
-		},
-        time_window: function() {
-            return get(this, 'config.time_window') * 1000;
-        }.property('config.time_window'),
-
-        time_window_offset: function() {
-            return get(this, 'config.time_window_offset') * 1000;
-        }.property('config.time_window_offset'),
-
-		aggregate_method: function(){
-			return get(this, 'config.aggregate_method')
-		}.property('config.aggregate_method'),
+        needs: ['serie', 'perfdata'],
 
         findItems: function() {
             var replace = false;
             var from = get(this, 'lastRefresh');
             var now = new Date().getTime();
-            var to = now - get(this, 'time_window_offset');
+            var to = now - get(this, 'config.time_window_offset');
 
             if(from === null) {
                 replace = true;
-                from = to - get(this, 'time_window') - get(this, 'time_window_offset');
+                from = to - get(this, 'config.time_window') - get(this, 'config.time_window_offset');
             }
 
             console.log('refresh:', from, to, replace);
@@ -70,99 +53,38 @@ function($, WidgetFactory) {
             console.groupEnd();
         },
         fetchMetrics: function(from, to, replace) {
-			var perfdata = get(this, 'controllers.perfdata');
+            var perfdata = get(this, 'controllers.perfdata');
             var metrics = get(this, 'config.metrics');
 
-			var aggregate_method = get(this, 'aggregate_method');
-			var aggregate_interval = get(this, 'aggregate_interval');
-			var vals = [];
-			var cmpt = 0;
+            var vals = [];
+            var cmpt = 0;
+            var mlength = metrics.length;
 
-			//test
+            for(var m = 0; m < mlength; m++) {
 
-			for(var m = 0; m < metrics.length; m++) {
+                var metricId = metrics[m];            
+                var total = 0;
 
-				var metricId = metrics[m];			
-				var total = 0;
+                var me = this;
+                perfdata.aggregate(metricId, from, to, "last", 0).then(function(result) {
 
-				var me = this;
-				perfdata.aggregate(metricId, from, to, aggregate_method, aggregate_interval).then(function(result) {
+                    var nmetric = result.total;
+                    var metrics = result.data;
 
-					var nmetric = result.total;
-					var metrics = result.data;
+                    var unit = result.data[0].meta.unit;
+                    set(me, 'unit', unit);
 
-					var unit = result.data[0].meta.unit;
-					set(me, 'unit', unit);
+                    var metric = result.data[i];
+                    var timestamp = metric.points[0][0];
+                    var value = metric.points[0][1];
+                    vals[cmpt] = value;
+                    cmpt += 1;
 
-					for(var i = 0; i < nmetric; i++) {
-						var metric = result.data[i];
-						var timestamp = metric.points[0][0];
-						var value = metric.points[0][1];
-						vals[cmpt] = value;
-						cmpt += 1;
-					}
-				});
-				
-			}
 
-            console.group('Generate Values:');
-            this.generateValue(vals, aggregate_method);
-            console.groupEnd();
-			
-        },
-        generateValue: function(vals, aggregate_method) {
-            console.group('Generate Value ProgressBar');
-			switch(aggregate_method) {
-				case "last":
-					set(this, 'value', 0);
-					break;
-				case "first":
-					set(this, 'value', 0);
-					break;
-				case "min":
-					set(this, 'value', 0);
-					break;
-				case "max":
-					set(this, 'value', 0);
-					break;
-				case "average":
-					set(this, 'value', 0);
-					break;
-				case "sum":
-					set(this, 'value', 0);
-					 break;
-				default:
-					set(this, 'value', 0);
-			} 
-			set(this, 'value', 75);
-
-			this.generateConfig();
-            console.groupEnd();
-        },
-        generateConfig: function() {
-            console.group('Generate Config ProgressBar');
-            var config = get(this, 'config');
-			set(this, "min_value", get(config, "min_value"));
-			set(this, "warn_value", get(config, "warn_value"));
-			set(this, "crit_value", get(config, "crit_value"));
-			set(this, "max_value", get(config, "max_value"));
-			set(this, "background_color", get(config, "background_color"));
-			set(this, "start_color", get(config, "start_color"));
-			set(this, "warn_color", get(config, "warn_color"));
-			set(this, "critic_color", get(config, "critic_color"));
-			set(this, "thickness", get(config, "thickness"));
-			set(this, "vertical", get(config, "vertical"));
-			set(this, "height", get(config, "height"));
-			set(this, "label", get(config, "label"));
-			set(this, "labeldisplay", get(config, "labeldisplay"));
-			set(this, "labelalign", get(config, "labelalign"));
-			set(this, "labelvertical", get(config, "labelvertical"));
-			set(this, "show_value", get(config, "show_value"));
-			set(this, "labelwidth", 0);
-			console.groupEnd();
+                });
+            }
         }
     }, widgetOptions);
-
 
     return widget;
 });
--- var/www/canopsis/uibase/components/progressbar/component.js
+++ var/www/canopsis/uibase/components/progressbar/component.js
@@ -8,75 +8,65 @@
         set = Ember.set;
 
     Application.ComponentProgressbarComponent = Ember.Component.extend({
-    classNames: 'progress',
+        classNames: 'progress',
 
-    init:function(){
-        this._super();
-    },
+        style_bar: function(){
+            var color = "background: " + this.getcolor() + ";";
+            return color + "width: " + this.get("percent") + "%;";
+        }.property("style_bar"),
 
-    width_label: function(){
-		//var show_label = this.get("labeldisplay");
-		/*
-		if (show_label) {
-        	return "display:none; background:#ffffff; width: %@%; float:left;".fmt(this.get("labelwidth"));
-		}
-		*/
-		return "display:none;";
-    }.property("width_label"),
+        getcolor: function(){
+            var background_color = this.get("background_color");
+            var warn_color = this.get("warn_color");
+            var critic_color = this.get("critic_color");
+            var valstatus = this.getstatus();
+            switch(valstatus){
+                case "critical":
+                    return critic_color;
+                    break;
+                case "warning":
+                    return warn_color;
+                    break;
+                default:
+                    return background_color;
+                    break;
+            }
+        },
 
-	style_bar: function(){
-		var color = "background: " + this.getcolor() + ";";
-        return color + "width: " + this.get("percent") + "%;";
-    }.property("style_bar"),
+        getstatus: function(){
+            var percent = this.get("percent");
+            if(percent > this.get("crit_value")){
+                return "critical";
+            } else if(percent > this.get("warn_value")){
+                return "warning";
+            } else {
+                return "complete"
+            }
+        },
+        
+        textstatus:function(){
+            return "(" + this.getstatus() + ")";
+        }.property("textstatus"),
 
-	style_span: function(){
-		return "display:none;";
-    }.property("style_span"),
+        textpercent:function(){
+            return "" + ""/*str(this.percent())*/ + "";
+        }.property("textpercent"),
 
-	getcolor: function(){
-		var background_color = this.get("background_color");
-		var warn_color = this.get("warn_color");
-		var critic_color = this.get("critic_color");
-		var valstatus = this.get("getstatus");
-		switch(valstatus){
-			case "critical":
-				return critic_color;
-				break;
-			case "warning":
-				return warn_color;
-				break;
-			default:
-				return background_color;
-				break;
-		}
-	},
+        percent:function(){
+            var value = this.get("value") - this.get("min_value");
+            var max = this.get("max_value") - this.get("min_value");
+            var percent =  Math.ceil(value/max * 100);
+            return percent;
+        }.property("percent")
 
-	getstatus: function(){
-		var percent = this.get("percent");
-		if(percent > this.get("crit_value")){
-			return "critical";
-		} else if(percent > this.get("warn_value")){
-			return "warning";
-		} else {
-			return "complete"
-		}
-	},
-	
-	textstatus:function(){
-		return "(" + this.getstatus() + ")";
-	}.property("textstatus"),
+    });
 
-	textpercent:function(){
-		return "" + ""/*str(this.percent())*/ + "";
-	}.property("textpercent"),
+    Ember.Application.initializer({
+        name:"component-progressbar",
+        initialize: function(container, application) {
+            application.register('component:component-progressbar', component);
+        }
+    });
 
-    percent:function(){
-        var value = this.get("value") - this.get("min_value");
-        var max = this.get("max_value") - this.get("min_value");
-        var percent =  Math.ceil(value/max * 100);
-        return percent;
-    }.property("percent")
-
-  });
-  return   Application.ComponentProgressbarComponent;
+    return component;
 });
