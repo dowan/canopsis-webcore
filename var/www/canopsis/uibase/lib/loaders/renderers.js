ec98417076f4bc0213a551a60d94621d83e384d1
Gwenael Pluchon <accounts@gwenp.fr>
2015-05-25 15:23:04 +0200

    moved some components and renderers from uibase to monitoring brick

--- var/www/canopsis/uibase/lib/loaders/renderers.js
+++ var/www/canopsis/uibase/lib/loaders/renderers.js
@@ -18,7 +18,6 @@
 */
 
 var renderers = [
-    { name: 'renderer-ack', template: 'canopsis/uibase/renderers/ack.html'},
     { name: 'renderer-actionfilter', template: 'canopsis/uibase/renderers/actionfilter.html'},
     { name: 'renderer-boolean', template: 'canopsis/uibase/renderers/boolean.html'},
     { name: 'renderer-cfilter', template: 'canopsis/uibase/renderers/cfilter.html'},
@@ -26,23 +25,16 @@ var renderers = [
     { name: 'renderer-color', template: 'canopsis/uibase/renderers/color.html'},
     { name: 'renderer-conf', template: 'canopsis/uibase/renderers/conf.html'},
     { name: 'renderer-crecord-type',template: 'canopsis/uibase/renderers/crecord-type.html'},
-    { name: 'renderer-criticity', template: 'canopsis/uibase/renderers/criticity.html'},
-    { name: 'renderer-eventselector', template: 'canopsis/uibase/renderers/eventselector.html'},
     { name: 'renderer-mail', template: 'canopsis/uibase/renderers/mail.html'},
     { name: 'renderer-object', template: 'canopsis/uibase/renderers/object.html'},
     { name: 'renderer-percent', template: 'canopsis/uibase/renderers/percent.html'},
     { name: 'renderer-rights', template: 'canopsis/uibase/renderers/rights.html'},
     { name: 'renderer-richtext', template: 'canopsis/uibase/renderers/richtext.html'},
     { name: 'renderer-source', template: 'canopsis/uibase/renderers/source.html'},
-    { name: 'renderer-state', template: 'canopsis/uibase/renderers/state.html'},
-    { name: 'renderer-stateConnector', template: 'canopsis/uibase/renderers/stateConnector.html'},
-    { name: 'renderer-status', template: 'canopsis/uibase/renderers/status.html'},
     { name: 'renderer-subprocess', template: 'canopsis/uibase/renderers/subprocess.html'},
     { name: 'renderer-tags', template: 'canopsis/uibase/renderers/tags.html'},
     { name: 'renderer-timestamp', template: 'canopsis/uibase/renderers/timestamp.html'},
     { name: 'renderer-translator', template: 'canopsis/uibase/renderers/translator.html'},
-    { name: 'renderer-eventtype', template: 'canopsis/uibase/renderers/eventtype.html'},
-    { name: 'renderer-eventtimestamp', template: 'canopsis/uibase/renderers/eventtimestamp.html'},
     { name: 'renderer-recordinfopopup', template: 'canopsis/uibase/renderers/recordinfopopup.html'},
     { name: 'renderer-labelledlink', template: 'canopsis/uibase/renderers/labelledlink.hbs'}
 ];
