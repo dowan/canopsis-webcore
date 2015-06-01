8a4fbebde1e18d4946da3332bf117b2c3e34058d
fdemeulenaere@capensis.fr <fdemeulenaere@capensis.fr>
2015-06-01 09:45:42 +0200

    update branch

--- var/www/canopsis/widgetCalendar/adapters/calendardata.js
+++ var/www/canopsis/widgetCalendar/adapters/calendardata.js
@@ -1,26 +1,53 @@
-define([
-    'ember',
-    'app/application',
-    'canopsis/canopsis-backend-ui-connector/adapters/baseadapter'
-], function(Ember, Application, BaseAdapter) {
 
-    var isNone = Ember.isNone,
-        get = Ember.get;
+define([
+    'jquery',
+    'ember',
+    'canopsis/canopsis-backend-ui-connector/adapters/baseadapter'
+], function($, Ember, BaseAdapter) {
+
+    var get = Ember.get,
+        set = Ember.set,
+        isNone = Ember.isNone;
+
+    var _upsertRecord = function(adapter, verb, store, type, record) {
+        var serializer = store.serializerFor(type.typeKey);
+        var data = serializer.serializeIntoHash({}, type, record, verb, { includeId: true
+        });
+
+        var query = {
+            data: {
+                'document': data
+            }
+        };
+
+        return adapter.ajax(adapter.buildURL(), verb, query);
+    };
 
     var adapter = BaseAdapter.extend({
 
-        buildURL: function(type, id) {
-            void(id);
+        buildURL: function(type, id, record) {
+            void(type);
+            void(record);
 
-            return '/calendar';
+            var result = '/calendar';
+
+            if (!isNone(id)) {
+                result += '/' + id;
+            }
+
+            return result;
         },
+
         createRecord: function(store, type, record) {
-            this._super();
+            return _upsertRecord(this, 'PUT', store, type, record);
+        },
+
+        updateRecord: function(store, type, record) {
+            return _upsertRecord(this, 'PUT', store, type, record);
         }
     });
 
-
-    loader.register('adapter:calendardata', adapter);
+    loader.register('adapter:vevent', adapter);
 
     return adapter;
 });
