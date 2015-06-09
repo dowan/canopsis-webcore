9f781a2af13e05b825581ca27287471f402a4e03
Gwenaël Pluchon <info@gwenp.fr>
2015-06-09 11:30:25 +0200

    first draft of widget duplication button

--- var/www/canopsis/core/controller/userview.js
+++ var/www/canopsis/core/controller/userview.js
@@ -20,13 +20,16 @@
 define([
     'ember',
     'app/lib/utils/forms',
+    'app/lib/utils/hash',
+    'app/lib/schemasregistry',
     'app/mixins/inspectableitem',
     'app/routes/userview',
     'app/serializers/userview'
-], function(Ember, formUtils, InspectableItem) {
+], function(Ember, formUtils, hashUtils, schemasregistry, InspectableItem) {
 
     var get = Ember.get,
         set = Ember.set,
+        isNone = Ember.isNone,
         __ = Ember.String.loc;
 
 
@@ -73,12 +76,12 @@ define([
                     var newWidgetWrapper = newWidgetWrappers[0];
 
                     console.log('onsubmit, adding widgetwrapper to containerwidget', newWidgetWrapper, containerController);
-                    console.log('containerwidget items', get(containerController, 'content.items.content'));
+                    console.log('containerwidget items', get(containerController, 'model.items.content'));
                     //FIXME wrapper does not seems to have a widget
-                    get(containerController, 'content.items.content').pushObject(newWidgetWrapper);
+                    get(containerController, 'model.items.content').pushObject(newWidgetWrapper);
 
                     console.log("saving view");
-                    get(userviewController, 'content').save().then(function() {
+                    get(userviewController, 'model').save().then(function() {
                         userviewController.trigger('refreshView');
                     });
                 });
@@ -88,17 +91,70 @@ define([
                 });
             },
 
-            duplicateWidgetAndContent: function(widgetModel) {
-                window.$F(widgetModel);
+            duplicateWidgetAndContent: function(widgetwrapperModel, containerModel) {
+                console.error('duplicateWidgetAndContent', widgetwrapperModel, containerModel);
+                this.copyWidget(widgetwrapperModel, containerModel);
+
+                this.get('model').save();
+
             },
 
             refreshView: function() {
                 console.log('refresh view');
                 this.trigger('refreshView');
             }
+        },
+        copyWidget: function(widgetwrapperModel, containerModel) {
+            var widgetwrapperJson = cleanRecord(widgetwrapperModel.toJSON());
+            widgetwrapperJson.widget = null;
+            var duplicatedWrapper = this.store.createRecord('widgetwrapper', widgetwrapperJson);
+
+            var widgetJson = cleanRecord(widgetwrapperModel.get('widget').toJSON());
+            console.error(widgetJson);
+
+            // if(!widgetJson.items) {
+            //     widgetJson.items = null;
+            // }
+
+            var duplicatedWidget = this.store.createRecord(widgetJson.xtype, widgetJson);
+
+            // if(!isNone(widgetwrapperModel.get('widget.items'))) {
+                // var items = widgetwrapperModel.get('widget.items');
+                // for (var i = 0; i < items.length; i++) {
+                    // var subWrapperModel = items.objectAt(i);
+                    // this.copyWidget(subWrapperModel, duplicatedWidget);
+                // }
+            // }
+
+            duplicatedWrapper.set('widget',  duplicatedWidget);
+            containerModel.get('items.content').pushObject(duplicatedWrapper);
         }
     });
 
+
+
+    function cleanRecord(recordJSON) {
+        for (var key in recordJSON) {
+            var item = recordJSON[key];
+            //see if the key need to be cleaned
+            if(key === 'id' || key === '_id' || key === 'widgetId' || key === 'preference_id') {
+                delete recordJSON[key];
+            }
+
+            //if this item is an object, then recurse into it
+            //to remove empty arrays in it too
+            if (typeof item === 'object') {
+                cleanRecord(item);
+            }
+        }
+
+        if(recordJSON !== null && recordJSON !== undefined) {
+            recordJSON['id'] = hashUtils.generateId('newitem');
+            recordJSON['_id'] = recordJSON['id'];
+        }
+
+        return recordJSON;
+    }
     loader.register('controller:userview', controller);
 
     return controller;
