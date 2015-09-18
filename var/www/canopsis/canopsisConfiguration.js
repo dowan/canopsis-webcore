78b034a6df6f329621c6031ff1636aa2ec3412d5
Gwenaël Pluchon <gpluchon@capensis.fr>
2015-09-18 10:26:10 +0200

    disable canopsisConfiguration debug mode

--- var/www/canopsis/canopsisConfiguration.js
+++ var/www/canopsis/canopsisConfiguration.js
@@ -110,7 +110,7 @@ define(['ember-lib', 'ember-data-lib'], function () {
      * from the whole application through the namespace Canopsis.conf.PROPERTY
      */
     var canopsisConfiguration = {
-        DEBUG: true,
+        DEBUG: false,
         VERBOSE: 1,
         showPartialslots: false,
         DISPLAY_SCHEMA_MANAGER: true,
