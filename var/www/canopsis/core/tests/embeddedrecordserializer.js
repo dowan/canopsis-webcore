501e525c69258dbb22711641d6f9309ce9353533
Gwenaël Pluchon <gpluchon@capensis.fr>
2015-09-24 15:21:50 +0200

    Revert "add fake embeddedrecordserializer test"
    This reverts commit de02d13b4b2c3f907eda80737c04f9fe51627d22.

--- var/www/canopsis/core/tests/embeddedrecordserializer.js
+++ var/www/canopsis/core/tests/embeddedrecordserializer.js
@@ -1,15 +1,15 @@
-module('Ack workflow', {
-    setup: function () {
-        // App.reset();
-    },
-    teardown: function() {
-        // $.mockjax.clear();
-    }
-});
+// module('Ack workflow', {
+//     setup: function () {
+//         // App.reset();
+//     },
+//     teardown: function() {
+//         // $.mockjax.clear();
+//     }
+// });
 
-test('Test embeddedrecordserializer', function() {
+// test('Test embeddedrecordserializer', function() {
 
-    var userviewSerializer = App.__container__.lookup('serializer:userview');
+//     var userviewSerializer = App.__container__.lookup('serializer:userview');
 
-    equal(1, 1, 'Expect the event ouptut is equal to "reason"' + userviewSerializer);
-});
+//     equal(1, 1, 'Expect the event ouptut is equal to "reason"' + userviewSerializer);
+// });
