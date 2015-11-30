9339eb6752b2df26b81a8ecabf8de64bd7535709
David Delassus <david.jose.delassus@gmail.com>
2015-11-30 09:46:46 +0100

    :fix: final fix for requirejs configuration

--- var/www/login/login.conf.js
+++ var/www/login/login.conf.js
@@ -21,13 +21,18 @@ require.config({
     waitSeconds: 40,
     baseUrl: '/static/',
     paths: {
+        'app': 'canopsis/core',
         'jquery': 'canopsis/core/lib/wrappers/jquery',
+        'bootstrap': 'canopsis/uibase/lib/externals/bootstrap/dist/js/bootstrap.min',
         'handlebars': 'canopsis/core/lib/externals/handlebars/handlebars',
         'ember-template-compiler': 'canopsis/core/lib/externals/min/ember-template-compiler',
         'ember-lib': 'canopsis/core/lib/externals/min/ember.debug',
         'ember-data-lib': 'canopsis/core/lib/externals/min/ember-data'
     },
     shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        },
         'ember-lib': {
             deps: ['jquery', 'ember-template-compiler', 'handlebars']
         },
@@ -39,6 +44,7 @@ require.config({
 
 define([
     'canopsis/canopsisConfiguration',
+    'bootstrap'
 ], function(canopsisConfiguration) {
     console.log(canopsisConfiguration);
     //Set page title
