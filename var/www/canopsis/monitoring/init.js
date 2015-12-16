a2ce2990e619113821746b104311b69f7e201de2
Gwenaël Pluchon <gpluchon@capensis.fr>
2015-12-16 17:12:46 +0100

    remove loaders for monitoring brick

--- var/www/canopsis/monitoring/init.js
+++ var/www/canopsis/monitoring/init.js
@@ -19,14 +19,63 @@
 
 /** @module canopsis.frontend.monitoring */
 
+require.config({
+    paths: {
+        'components/component-cfiltereditor': 'canopsis/monitoring/src/components/cfiltereditor/template',
+        'components/component-ack': 'canopsis/monitoring/src/components/ack/template',
+        'components/component-stateeditor': 'canopsis/monitoring/src/components/stateeditor/template',
+        'components/component-eventselector': 'canopsis/monitoring/src/components/eventselector/template',
+        'components/component-statemapping': 'canopsis/monitoring/src/components/statemapping/template',
+        'editor-metricselector': 'canopsis/monitoring/src/editors/metricselector',
+        'renderer-ack': 'canopsis/monitoring/src/renderers/ack',
+        'renderer-crecord-type': 'canopsis/monitoring/src/renderers/crecord-type',
+        'renderer-criticity': 'canopsis/monitoring/src/renderers/criticity',
+        'renderer-eventselector': 'canopsis/monitoring/src/renderers/eventselector',
+        'renderer-state': 'canopsis/monitoring/src/renderers/state',
+        'renderer-stateConnector': 'canopsis/monitoring/src/renderers/stateConnector',
+        'renderer-status': 'canopsis/monitoring/src/renderers/status',
+        'renderer-eventtype': 'canopsis/monitoring/src/renderers/eventtype',
+        'renderer-eventtimestamp': 'canopsis/monitoring/src/renderers/eventtimestamp',
+        'actionbutton-editurlfield': 'canopsis/monitoring/src/templates/actionbutton-editurlfield',
+        'weather': 'canopsis/monitoring/src/widgets/weather/template'
+    }
+});
+
 define([
-    'canopsis/monitoring/src/lib/loaders/editors',
-    'canopsis/monitoring/src/lib/loaders/widgets',
-    'canopsis/monitoring/src/lib/loaders/components',
-    'canopsis/monitoring/src/lib/loaders/helpers',
-    'canopsis/monitoring/src/lib/loaders/mixins',
-    'canopsis/monitoring/src/lib/loaders/templates',
-    'canopsis/monitoring/src/lib/loaders/renderers'
+    'ehbs!components/component-cfiltereditor',
+    'ehbs!components/component-ack',
+    'ehbs!components/component-stateeditor',
+    'ehbs!components/component-eventselector',
+    'ehbs!components/component-statemapping',
+    'ehbs!editor-metricselector',
+    'ehbs!renderer-ack',
+    'ehbs!renderer-crecord-type',
+    'ehbs!renderer-criticity',
+    'ehbs!renderer-eventselector',
+    'ehbs!renderer-state',
+    'ehbs!renderer-stateConnector',
+    'ehbs!renderer-status',
+    'ehbs!renderer-eventtype',
+    'ehbs!renderer-eventtimestamp',
+    'ehbs!actionbutton-editurlfield',
+    'ehbs!weather',
+    'canopsis/monitoring/src/widgets/weather/controller',
+    'canopsis/monitoring/src/helpers/criticity',
+    'canopsis/monitoring/src/helpers/stateview',
+    'canopsis/monitoring/src/helpers/statusview',
+    'canopsis/monitoring/src/helpers/recordcanbeack',
+    'canopsis/monitoring/src/mixins/sendevent',
+    'canopsis/monitoring/src/mixins/recordinfopopup',
+    'canopsis/monitoring/src/mixins/history',
+    'canopsis/monitoring/src/mixins/eventnavigation',
+    'canopsis/monitoring/src/mixins/eventhistory',
+    'canopsis/monitoring/src/mixins/infobutton',
+    'canopsis/monitoring/src/mixins/editurlfield',
+    'canopsis/monitoring/src/components/cfiltereditor/component',
+    'canopsis/monitoring/src/components/ack/component',
+    'canopsis/monitoring/src/components/stateeditor/component',
+    'canopsis/monitoring/src/components/eventselector/component',
+    'canopsis/monitoring/src/components/statemapping/component'
 ], function () {
 
 });
