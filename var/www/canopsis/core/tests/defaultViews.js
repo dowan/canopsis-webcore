289e3f4291b06a974225c61d6773f8ef24a62c4f
Gwenaël Pluchon <gpluchon@capensis.fr>
2016-01-05 17:53:29 +0100

    reactivate default system views tests

--- var/www/canopsis/core/tests/defaultViews.js
+++ var/www/canopsis/core/tests/defaultViews.js
@@ -1,23 +1,23 @@
-// module('default views');
+module('default views');
 
-// test('Test app_header', function() {
+test('Test app_header', function() {
 
-//     visit('/userview/view.app_header');
+    visit('/userview/view.app_header');
 
-//     andThen(function() {
-//         equal(find('.tab-content .nav-tabs').length, 1, 'There is a bootstrap nav-tabs in the view');
-//     });
-// });
+    andThen(function() {
+        equal(find('.tab-content .nav-tabs').length, 1, 'There is a bootstrap nav-tabs in the view');
+    });
+});
 
-// test('Test app_footer', function() {
+test('Test app_footer', function() {
 
-//     visit('/userview/view.app_footer');
+    visit('/userview/view.app_footer');
 
-//     andThen(function() {
-//         equal(
-//             find('.tab-content img:first')[0].src,
-//             'http://localhost:8082/static/canopsis/media/sakura.png',
-//             'Canopsis logo is present'
-//         );
-//     });
-// });
+    andThen(function() {
+        equal(
+            find('.tab-content img:first')[0].src,
+            'http://localhost:8082/static/canopsis/media/sakura.png',
+            'Canopsis logo is present'
+        );
+    });
+});
