89366168a9054d6ff0a6c5268b62bff7910948fc
Gwenaël Pluchon <gpluchon@capensis.fr>
2016-04-21 10:25:29 +0200

    push default bricks docs into the main doc folder

--- doc/_static/bricks-documentation/doc-config.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var titleLink = document.querySelectorAll("nav h2 a");
-    var href = titleLink[0].href;
-    href = href.substring(0, href.lastIndexOf("/"));
-    href += '/../../../../index.html';
-
-    titleLink[0].href = href;
-});
