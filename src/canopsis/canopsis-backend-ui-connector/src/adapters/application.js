24a02de42616a8882f02e80c584105556bd240e8
Gwenaël Pluchon <gpluchon@capensis.fr>
2016-09-16 10:02:03 +0200

    build doc for canopsis-backend-ui-connector

---
+++ src/canopsis/canopsis-backend-ui-connector/src/adapters/application.js
@@ -0,0 +1,26 @@
+//TODO move ApplicationAdapter from requirejsmodules to here
+
+/**
+ * @adapter Application
+ * @description Main adapter for Canopsis. This adapter is used by default.
+ *
+ * > Note that the current adapters design is not yet clean. There should be one adapter per data source, not one per data type.
+ *
+ */
+         /**
+         * @method ajax
+         * Override allowing to use the promisemanager
+         */
+         /**
+         * @method buildURL
+         * @argument type
+         * @argument id
+         * @description builds the url for any kind of request. Override this on your adapter if you need to make queries to specific urls.
+         * @return {string} the url of the query
+         */
+        /**
+         * @method createRecord
+         */
+        /**
+         * @method updateRecord
+         */
